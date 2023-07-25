cf27ddcb277ec5e584ab7e0a6ea5f86fe7a1f99a
Aaron_Zhou <clark1729@outlook.com>
2023-07-25 12:27:28 +0800

    feat: `startViewTransition` accepts an optional callback
    - `startViewTransition` accepts an optional callback, which returns a promise. the view transition starts once the promise is resolved.
    - `viewTransition.updateCallbackDone` is provided in the returned `ViewTransition` object, which is the same as the native `ViewTransition` object

--- src/transition.ts
+++ src/transition.ts
@@ -1,18 +1,23 @@
 interface ViewTransition {
+    updateCallbackDone: Promise<void>
     ready: Promise<void>
     finished: Promise<void>
     skipTransition: () => void
 }
 
-export async function startViewTransition(): Promise<ViewTransition> {
+export async function startViewTransition(callback?: () => Promise<void>): Promise<ViewTransition> {
     const viewTransition = {} as ViewTransition
     // @ts-expect-error startViewTransition is not existed on document yet
     if (document.startViewTransition) {
         await new Promise<void>((resolve) => {
             // @ts-expect-error startViewTransition is not existed on document yet
-            const nativeViewTransition = document.startViewTransition(() => {
+            const nativeViewTransition = document.startViewTransition(async () => {
+                if (callback) {
+                    await callback()
+                }
                 resolve()
             })
+            viewTransition.updateCallbackDone = nativeViewTransition.updateCallbackDone
             viewTransition.ready = nativeViewTransition.ready
             viewTransition.finished = nativeViewTransition.finished
             viewTransition.skipTransition =
